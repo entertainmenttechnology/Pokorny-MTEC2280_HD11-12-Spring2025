8fea23b3538388a5886e2698c52231e6b03b3f47
ianpokorny <ianpokorny@gmail.com>
2025-03-05 21:40:09 -0500

    Update S05-AnalogRead.ino

--- examples/S05-AnalogRead/S05-AnalogRead.ino
+++ examples/S05-AnalogRead/S05-AnalogRead.ino
@@ -1,7 +1,13 @@
 /*
 <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+////////////////////////////////////
 //      "ANALOG READ"             //
 //  ANALOG TO DIGITAL CONVERSION  //
+////////////////////////////////////
+- ADC capable pins of the MicroController can read analog voltage and convert into integer numbers
+- NOTE: ADC Voltage range must be within 0-3.3V, otherwise you may damage device
+- You can connect either of our analog sensors' outputs to potPin: 
+    - Pot or Photocell [refer to schematic for wiring example]
 REFERENCE:
 - https://docs.arduino.cc/language-reference/en/functions/analog-io/analogRead/
 - https://docs.arduino.cc/language-reference/en/functions/analog-io/analogReadResolution/
@@ -11,7 +17,7 @@ const int potPin = 1;
 
 void setup() 
 {
-  analogReadResolution(12);   //default ADC resolution for ESP32 is 12-bit [0-4095]
+  analogReadResolution(12);   //default ADC resolution for ESP32 is 12-bit [range of 0-4095]
 
   Serial.begin(115200); //start serial comms
 }
