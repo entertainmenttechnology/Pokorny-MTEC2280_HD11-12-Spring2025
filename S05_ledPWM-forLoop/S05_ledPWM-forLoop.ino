c51826a87303de030cda8da57be8f4feb02db016
ianpokorny <ianpokorny@gmail.com>
2025-03-05 23:25:16 -0500

    add more s5 examples

---
+++ S05_ledPWM-forLoop/S05_ledPWM-forLoop.ino
@@ -0,0 +1,45 @@
+/*
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+||||||||||||||||||||||||||||||||||||||||||
+  ||   "LED PWM DIMMING: FOR LOOP"    ||
+  ||       by Ian Pokorny             ||
+  ||         MTEC-2280                ||
+||||||||||||||||||||||||||||||||||||||||||
+- Demo of using analogWrite & for loops to dim an LED off and on.
+    - Connect LED circuit to GPIO Pin 4
+    - NOTE: Don't forget your current-limiting resistor!
+
+REFERENCE:
+- https://docs.arduino.cc/language-reference/en/functions/analog-io/analogWrite/
+- https://docs.arduino.cc/language-reference/en/structure/control-structure/for/
+<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+*/
+
+const int ledPin = 4;
+int ms = 30; //adjust to speed up/slow down
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+
+  for (int i = 0; i < 255; i++)  //starting from 0, incrementing to 255
+  {
+    analogWrite(ledPin, i); //send PWM signal @ current i value in for loop
+    delay(ms);
+    Serial.printf("FOR LOOP 1: %i \n",i);
+  }
+
+  for (int i = 255; i > 0; i--) //starting from 255, decrementing to 0
+  {
+    analogWrite(ledPin, i); //send PWM signal @ current i value in for loop
+    delay(ms);
+    Serial.printf("FOR LOOP 2: %i \n",i);
+  }
+
+
+   
+}
